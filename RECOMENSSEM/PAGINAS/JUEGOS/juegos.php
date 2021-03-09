d1f737fae18e2e70b91c7d1f4052a174287f847b
ArnauPobladorNavarro-examen <arnautrv@gmail.com>
2021-03-09 13:16:36 +0100

    asd

--- RECOMENSSEM/PAGINAS/JUEGOS/juegos.php
+++ RECOMENSSEM/PAGINAS/JUEGOS/juegos.php
@@ -1,5 +1,4 @@
 <?php
-
 	include_once("../../php_partials/menu.php");
 	require_once("../../php_librarys/bd.php");
 	
@@ -18,6 +17,13 @@
 		}
 	}
 
+	if( isset($_SESSION['admin']) ){
+		$admin = true;
+	}else{
+		$admin = false;
+	}
+
+	$listaJuegos = selectJuegos();
 
 ?>
 
@@ -38,6 +44,10 @@
 	</head>
 	<body>
 	<br>
+		<?php
+			if( !$admin ){
+		?>
+
 
 		<div class="container">
 			<div class="row">
@@ -53,7 +63,7 @@
 									
 							<img src="/RECOMENSSEM/media/FotoJuegoArnau.png" class="card-img-top" alt="Enviar formulario"> 
 							<div class="card-body">
-								<strong class="lang" key="Juego1"> Juego 1</strong>   
+								<strong class="lang" key="Juego1"> <?php echo $lang['Juego1'] ?></strong>   
 								<br>    
 							</div>
 						
@@ -75,7 +85,7 @@
 					<img src="/RECOMENSSEM/media/FotoJuegoAlbert.png" class="card-img-top" alt="Enviar formulario"> 
 
 							<div class="card-body">
-								<strong class="lang" key="Juego2"> Juego 2</strong>   
+								<strong class="lang" key="Juego2"> <?php echo $lang['Juego2'] ?></strong>   
 								<br>    
 							</div>
 						
@@ -96,7 +106,7 @@
 									
 							<img src="/RECOMENSSEM/media/FotoJuegoDani.png" class="card-img-top" alt="Enviar formulario"> 
 							<div class="card-body">
-								<strong class="lang" key="Juego3"> Juego 3</strong>   
+								<strong class="lang" key="Juego3"> <?php echo $lang['Juego3'] ?></strong>   
 								<br>    
 							</div>
 						
@@ -115,7 +125,7 @@
 						
 							<img src="/RECOMENSSEM/media/FotoJuegoCarlos.png" class="card-img-top" alt="Enviar formulario"> 
 							<div class="card-body">
-								<strong class="lang" key="Juego4"> Juego 4</strong>   
+								<strong class="lang" key="Juego4"> <?php echo $lang['Juego4'] ?></strong>   
 								<br>    
 							</div>
 			
@@ -127,22 +137,93 @@
 
 		</div>
 
+		<?php
+			}else{
+		?>
+
+			<div class="card" >
+				<div class="card-body">
+					<form action="/RECOMENSSEM/php_controllers/JuegosController.php" method="POST">	
+
+			
+						<label for="cbxJuego"><?php echo $lang['ModificarPuntosJuego'] ?></label>
+						<select name="cbxJuego" id="cbxJuego">
+							<option value="1"> <?php echo $lang['Juego1'] ?> </option>
+							<option value="2"> <?php echo $lang['Juego2'] ?> </option>
+							<option value="3"> <?php echo $lang['Juego3'] ?> </option>
+							<option value="4"> <?php echo $lang['Juego4'] ?> </option>
+						</select>
+					
+						<input required placeholder="Puntos" type="number" id="puntos" name="puntos" min="1" style="margin:0;" >
+						<label> <?php echo $lang['SaveGame'] ?> </label>
+						<button class="btn btn-outline-primary" type="submit" name="UpdateJuego" style="vertical-align: inherit;"> <i class="fas fa-save"></i> </button>
+				
+					
+
+					</form>  
+				
+			
+				<table class="table table-responsive table-bordered" style="text-align:center">
+					<thead>
+						<tr>
+							<th><?php echo $lang['JuegoAdmin'] ?></th>
+							<th><?php echo $lang['JuegoPuntos'] ?></th>
+							<th><?php echo $lang['JuegoFoto'] ?></th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td> <?php echo $listaJuegos[0]["nombre"] ?> </td>
+							<td> <?php echo $listaJuegos[0]["puntos"] ?> </td>
+							<td> <div style="width: 200px; height: 100px;">
+								<img src="/RECOMENSSEM/media/FotoJuegoArnau.png" class="card-img-top" alt="Enviar formulario"> 
+							</div> </td> 
+						</tr>
+						<tr>
+						<td> <?php echo $listaJuegos[1]["nombre"] ?> </td>
+							<td> <?php echo $listaJuegos[1]["puntos"] ?> </td>
+							<td> <div style="width: 200px; height: 100px;">
+								<img src="/RECOMENSSEM/media/FotoJuegoAlbert.png" class="card-img-top" alt="Enviar formulario"> 
+							</div> </td> 
+						</tr>
+						<tr>
+						<td> <?php echo $listaJuegos[2]["nombre"] ?> </td>
+							<td> <?php echo $listaJuegos[2]["puntos"] ?> </td>
+							<td> <div class="img"style="width: 200px; height: 100px;">
+								<img src="/RECOMENSSEM/media/FotoJuegoDani.png" class="card-img-top" alt="Enviar formulario"> 
+							</div> </td> 
+						</tr>
+						<tr>
+						<td> <?php echo $listaJuegos[3]["nombre"] ?> </td>
+							<td> <?php echo $listaJuegos[3]["puntos"] ?> </td>
+							<td> <div style="width: 200px; height: 100px;">
+								<img src="/RECOMENSSEM/media/FotoJuegoCarlos.png" class="card-img-top" alt="Enviar formulario"> 
+							</div> </td> 
+						</tr>
+					</tbody>
+				</table>
+
+			</div>
+		<?php
+			}
+		?>
+
 
     <!-- Modal -->
 	<div class="modal fade" id="modalJuegoSuperado" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">JUEGO </h5>
+                    <h5 class="modal-title" id="exampleModalLabel"><?php echo $lang['JuegoAdmin'] ?></h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
-					Juego superado, enorabuena, ya tiens 100 puntos mas, que los podras gastar en las ofertas. 
+					<?php echo $lang['JuegoNotText'] ?>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal"><?php echo $lang['BtnCerrar'] ?></button>
                 </div>
                 </div>
             </div>
@@ -154,16 +235,16 @@
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">JUEGO</h5>
+                    <h5 class="modal-title" id="exampleModalLabel"><?php echo $lang['JuegoAdmin'] ?></h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
-					Juego no superado, ha seguir intentandolo!!
+					<?php echo $lang['JuegoNotText2'] ?>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal"><?php echo $lang['BtnCerrar'] ?></button>
                 </div>
                 </div>
             </div>
@@ -175,16 +256,16 @@
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">JUEGO</h5>
+                    <h5 class="modal-title" id="exampleModalLabel"><?php echo $lang['JuegoAdmin'] ?></h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
-					Juego ya superado anteriormente, enhorabuena!
+					<?php echo $lang['JuegoNotText3'] ?>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal"><?php echo $lang['BtnCerrar'] ?></button>
                 </div>
                 </div>
             </div>
@@ -196,7 +277,6 @@
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="\RECOMENSSEM\js\script.js" charset="utf-8"></script>
 
 
 
@@ -236,5 +316,19 @@
 	}
 
 ?>
+
+
+
+
+<script>
+
+	function selecionarJuego(juego) {
+		document.getElementById("botonMenuJuegos").innerHTML = juego;
+		document.getElementById("botonMenuJuegos").value = juego;
+	
+	}
+
+</script>
+
 
 </html>
